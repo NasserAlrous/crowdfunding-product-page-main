7cb31a6628f3960c7bcfb796ab0d957359345584
Nasser <nasseralrous@gmail.com>
2021-02-25 09:35:59 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("submitForm").addEventListener("submit", SuccessMsg) = function SuccessMsg() {
+  alert('Thanks for your support! \nYour pledge brings us one step closer to sharing Mastercraft Bamboo Monitor Riser worldwide. You will get
+  an email once our campaign is completed.');
+}
